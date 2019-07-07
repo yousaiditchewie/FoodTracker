26827cd9042eea7a876205868e68e2a68460457a
Jacob Allen <jedijake@me.com>
2019-07-06 22:43:43 -0500

    cleanup project

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -88,7 +88,7 @@ import UIKit
         let emptyStar = generateImage(imageName: "emptyStar")
         let highlightedStar = generateImage(imageName: "highlightedStar")
         
-        for _ in 0..<starCount {
+        for index in 0..<starCount {
         
             // Create the button
             let button = UIButton()
@@ -104,6 +104,12 @@ import UIKit
             
             button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
             button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+            
+            // Set the accessibility label
+            button.accessibilityLabel = "Set \(index + 1) star rating"
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
             
             // Set up button action
             
@@ -123,6 +129,29 @@ import UIKit
         for (index, button) in ratingButtons.enumerated() {
             // If the index of a button is less than the rating, that button should be selected.
             button.isSelected = index < rating
+            
+            // Set the hint string for the currently selected star
+            let hintString: String?
+            if rating == index + 1 {
+                hintString = "Tap to reset the rating to zero."
+            } else {
+                hintString = nil
+            }
+            
+            // Calculate the value string
+            let valueString: String
+            switch (rating) {
+            case 0:
+                valueString = "No rating set."
+            case 1:
+                valueString = "1 star set."
+            default:
+                valueString = "\(rating) stars set."
+            }
+            
+            // Assign the hint string and value string
+            button.accessibilityHint = hintString
+            button.accessibilityValue = valueString
         }
     }
 }
