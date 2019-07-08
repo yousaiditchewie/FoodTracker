508c9b9fc42f1410074cfa8c5816e1ed57329eb0
Jacob Allen <jedijake@me.com>
2019-07-07 21:01:50 -0500

    Add and initialize Meal model

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,34 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by jacob on 7/7/19.
+//  Copyright © 2019 jacob. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0  {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+    
+}
